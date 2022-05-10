fb5c28d23550641cedcb360dcc24e3b2df3ad460
Zireux <96218233+Zireux@users.noreply.github.com>
2022-05-10 13:08:43 -0500

    create componente login
    inicio proyecto - add component "login" (se crea componente con texto centrado y background-color...)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
+import { MatSliderModule } from '@angular/material/slider';
+
+//Componentes
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
